cd9b0a263b4ad0db518cfd859860fc0296f52434
Hector <elector22@gmail.com>
2024-05-17 15:33:15 -0300

    aplicación redirect

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import axios from "../../config/axiosConfig";
 import { DataContext } from "../context/dataContext";
 import { toast } from 'react-toastify';
+import { Link } from "react-router-dom";
 
 export default function Logout() {
     const { setUser } = useContext(DataContext);
